e801d4302588a1afa73c25578dee9e137847853a
s-codedev <s.codedevelopment@gmail.com>
2021-08-02 12:40:10 +0700

    commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Form = ({inputColor, setInputColor, boxColors, setBoxColors}) => {
+  const allowKeys = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
   const inputColorHandler = (e) => {
     setInputColor(e.target.value)
   }
